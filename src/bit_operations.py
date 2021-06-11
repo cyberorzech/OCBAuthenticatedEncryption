4bdebcfed51ad6ed6933fee8ec31d447833cbc56
TrywialnyOrzech <natan.orze@gmail.com>
2021-06-11 10:51:05 +0200

    Add zeros() function

---
+++ src/bit_operations.py
@@ -0,0 +1,15 @@
+def zeros(n):
+    if n < 1:
+        raise ValueError("bit_operations: zeros: n argument should be greater than 0.")
+    result = ""
+    for x in range(0, n):
+        result += "0"
+    return result
+
+
+def main():
+    raise NotImplementedError("Not implemented yet. Use as package.")
+
+
+if __name__ == "__main__":
+    main()
--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,22 @@
 from src.aes import *
+from src.bit_operations import *
 import pytest
 
 from src.associated_data_hash import *
+
+
+class TestBit_Operations:
+    def test_zeros_with_invalid_arg(self):
+        with pytest.raises(ValueError) as valinfo:
+            zeros(0)
+        assert "n argument should be greater than 0" in str(valinfo.value)
+
+    def test_zeros_with_valid_arg(self):
+        n = 128
+        x = zeros(n)
+        assert len(x) == n
+        for i in range(0, len(x)):
+            assert x[i] == "0"
 
 
 class TestAssociated_Data_Hash:
@@ -21,11 +36,6 @@ class TestAssociated_Data_Hash:
 
 
 class TestAES:
-    # def test_AES128_when_data_is_too_long(self):
-    #     with pytest.raises(ValueError) as valError:
-    #         encryptDataAES128(data=b"Message that exceeds 16 bytes length")
-    #         assert "16 byte boundary" in str(valError.value)
-
     def test_AES128_when_data_is_valid(self):
         # AES encryption routine example
         messageToEncrypt = "#test@12345"
